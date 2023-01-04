6996a5a77cfadcba0a753339a60930aac2072fd3
felipeLeao18 <flpleao1807@gmail.com>
2023-01-04 12:16:15 -0300

    feat: add setup to vim tree

--- lua/flp/vim-tree/init.lua
+++ lua/flp/vim-tree/init.lua
@@ -1,4 +1,7 @@
 -- OR setup with some options
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
@@ -7,7 +10,31 @@ require("nvim-tree").setup({
     adaptive_size = true,
     mappings = {list = {{key = "u", action = "dir_up"}}}
   },
-  renderer = {group_empty = true},
+  renderer = {
+    group_empty = true,
+    indent_markers = {
+        enable = true,
+        icons = {
+          corner = "└ ",
+          edge = "│ ",
+          item = "│ ",
+          none = "  ",
+        },
+      },
+  },
+
   filters = {dotfiles = false},
-  git = {enable = true, ignore = false, show_on_dirs = true, timeout = 400}
+
+  git = {enable = true, ignore = false, show_on_dirs = true, timeout = 400},
+
+  diagnostics = {
+      enable = true,
+      show_on_dirs = false,
+      icons = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+      },
+    },
 })
